d8ced86cdb6d30c636fc130f4c31be9403d18c33
Faisal Rana <inquisitive@gmail.com>
2023-05-11 21:32:15 -0700

    netflify update

--- src/spotify-service.js
+++ src/spotify-service.js
@@ -1,51 +1,60 @@
 export default class SpotifyService {
   static async getToken() {
+    console.log("test");
+    console.log(process.env.CLIENT_ID);
+    console.log(process.env.CLIENT_SECRET);
     try {
-      const response = await fetch ('https://accounts.spotify.com/api/token', {
-        method: 'POST',
+      const response = await fetch("https://accounts.spotify.com/api/token", {
+        method: "POST",
         headers: {
-          'Content-Type' : 'application/x-www-form-urlencoded', 
-          'Authorization' : 'Basic ' + btoa(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`)
+          "Content-Type": "application/x-www-form-urlencoded",
+          Authorization:
+            "Basic " +
+            btoa(`${process.env.CLIENT_ID}:${process.env.CLIENT_SECRET}`),
         },
-        body: 'grant_type=client_credentials'
+        body: "grant_type=client_credentials",
       });
       if (!response.ok) {
         throw Error(response.statusText);
       }
       const token = await response.json();
       return token.access_token;
-    } catch(error) {
+    } catch (error) {
       return error.message;
     }
-
   }
-  static async getSearch(token,genre,years) {
+  static async getSearch(token, genre, years) {
     try {
-      const response = await fetch(`https://api.spotify.com/v1/search?q=year%3A${years[0]}-${years[1]}%20genre%3A${genre}&type=track`, {
-        method: 'GET',
-        headers: { 'Authorization' : 'Bearer ' + token}
-      });
+      const response = await fetch(
+        `https://api.spotify.com/v1/search?q=year%3A${years[0]}-${years[1]}%20genre%3A${genre}&type=track`,
+        {
+          method: "GET",
+          headers: { Authorization: "Bearer " + token },
+        }
+      );
       if (!response.ok) {
         throw Error(response.statusText);
       }
       return await response.json();
-    } catch(error){
+    } catch (error) {
       return error.message;
     }
   }
   static async getGenres(token) {
     try {
-      const response = await fetch(`https://api.spotify.com/v1/recommendations/available-genre-seeds`, {
-        method: 'GET',
-        headers: { 'Authorization' : 'Bearer ' + token}
-      });
+      const response = await fetch(
+        `https://api.spotify.com/v1/recommendations/available-genre-seeds`,
+        {
+          method: "GET",
+          headers: { Authorization: "Bearer " + token },
+        }
+      );
       if (!response.ok) {
         throw Error(response.statusText);
       }
       return await response.json();
-    } catch(error){
+    } catch (error) {
       return error.message;
     }
   }
 }
-
