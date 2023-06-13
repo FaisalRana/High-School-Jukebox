0d9abecc744f70b47593d575c2e51c1c9fa521da
Faisal Rana <inquisitive@gmail.com>
2023-06-13 13:26:25 -0700

    added env variables

--- src/spotify-service.js
+++ src/spotify-service.js
@@ -5,9 +5,9 @@ export default class SpotifyService {
     const client_secret = process.env.REACT_APP_CLIENT_SECRET;
     console.log(client_id);
     console.log(client_secret);
-    // console.log(process.env.VITE_CLIENT_ID);
-    // console.log(process.env.VITE_CLIENT_SECRET);
-    // console.log(process.env.VITE_REACT_APP_TEST);
+    console.log(process.env.VITE_CLIENT_ID);
+    console.log(process.env.VITE_CLIENT_SECRET);
+    console.log(process.env.VITE_REACT_APP_TEST);
 
     try {
       const response = await fetch("https://accounts.spotify.com/api/token", {
