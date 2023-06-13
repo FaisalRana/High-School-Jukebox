32ed1f8a8f209f436a3e8a857b4e9f5572f8b130
Faisal Rana <inquisitive@gmail.com>
2023-06-13 13:07:17 -0700

    removed console.logs

--- src/spotify-service.js
+++ src/spotify-service.js
@@ -1,13 +1,13 @@
 export default class SpotifyService {
   static async getToken() {
-    console.log("test");
+    // console.log("test");
     const client_id = process.env.REACT_APP_CLIENT_ID;
     const client_secret = process.env.REACT_APP_CLIENT_SECRET;
-    console.log(client_id);
-    console.log(client_secret);
-    console.log(process.env.VITE_CLIENT_ID);
-    console.log(process.env.VITE_CLIENT_SECRET);
-    console.log(process.env.VITE_REACT_APP_TEST);
+    // console.log(client_id);
+    // console.log(client_secret);
+    // console.log(process.env.VITE_CLIENT_ID);
+    // console.log(process.env.VITE_CLIENT_SECRET);
+    // console.log(process.env.VITE_REACT_APP_TEST);
 
     try {
       const response = await fetch("https://accounts.spotify.com/api/token", {
